98cc02c5d410a34a569da0f509601a70886a8cec
SD-13 <sujaydey641@gmail.com>
2022-09-06 14:15:54 +0530

    update url stuff

--- frontend/src/agent.js
+++ frontend/src/agent.js
@@ -5,8 +5,9 @@ const superagent = superagentPromise(_superagent, global.Promise);
 
 const API_ROOT =
   process.env.NODE_ENV !== "production"
-    ? "http://localhost:3000"
+    ? "http://localhost:3000/api"
     : process.env.BACKEND_URL;
+
 
 
 const encode = encodeURIComponent;
